7bee27d43cfa0e300889a1c4541d4a7613e6a3d8
Andriy Borkovich <borkovich25andri@gmail.com>
2024-11-21 01:08:13 +0200

    set up infra project

---
+++ src/Shared/ProjectManager.Infrastructure/InfrastructureRegistration.cs
@@ -0,0 +1,13 @@
+﻿using Amazon.S3;
+using Microsoft.Extensions.DependencyInjection;
+using ProjectManager.Infrastructure.Services;
+
+namespace ProjectManager.Infrastructure;
+
+public static class InfrastructureRegistration
+{
+    public static void AddInfra(this IServiceCollection services)
+    {
+        services.AddSingleton<IFileStorageService, S3FileStorageService>();
+    }
+}
